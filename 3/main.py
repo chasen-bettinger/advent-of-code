4d2f0f98177eb75211813c4cdf27e9f526e13e81
Chasen Bettinger <bettingerchasen@gmail.com>
2022-12-06 21:41:53 -0500

    day 3 part two solution

--- 3/main.py
+++ 3/main.py
@@ -2,30 +2,48 @@ import fileinput
 
 total=0
 character_list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+group=[]
+
 with fileinput.input("input.txt") as input:
     for line in input:
         line=line.replace("\n", "")
 
-        # find the length of the string
-        # assumption: length of line is always even
-        mid_point_of_components=int(len(line) / 2)
-        # split the string in half
-        first_half_of_components=line[:mid_point_of_components]
-        second_half_of_components=line[mid_point_of_components:]
+        group.append(line)
 
-        # iterate through both parts
-        #   in the first iteration, store the results
-        #   in the second iteration, look for duplicates
-        duplicate_component=None
-        for component in second_half_of_components:
-            if component in first_half_of_components:
-                duplicate_component=component
-                break
+        if len(group) is not 3:
+            continue
+
+        all_group_frequency={}
+        for group_line in group:
+            group_additions=[]
+
+            for letter in group_line:
+
+                if letter in group_additions:
+                    continue
+
+                group_additions.append(letter)
+
+                if letter not in all_group_frequency:
+                    all_group_frequency[letter] = 0
+
+                all_group_frequency[letter] = all_group_frequency[letter] + 1
                 
-        # if there is a duplicate, find its priority
-        is_duplicate_component_uppercase=duplicate_component.isupper()
 
-        component_to_search=duplicate_component.lower()
+        sorted_frequencies = {key: val for key, val in sorted(all_group_frequency.items(), key = lambda ele: ele[1], reverse = True)}
+        target_letter=None
+
+        for key in sorted_frequencies:
+            frequency=sorted_frequencies[key]
+
+            if frequency is 3:
+                target_letter=key
+                break
+
+        # if there is a duplicate, find its priority
+        is_duplicate_component_uppercase=target_letter.isupper()
+
+        component_to_search=target_letter.lower()
         character_points = character_list.index(component_to_search) + 1
 
         if is_duplicate_component_uppercase:
@@ -33,5 +51,6 @@ with fileinput.input("input.txt") as input:
 
         # add the priority to the running sum
         total += character_points
+        group=[]
 
 print(total)
